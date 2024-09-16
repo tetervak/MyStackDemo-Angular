7402eb4676cc9d6b17ec5c397e4da88efff34bdc
Alex Tetervak <tetervak@sheridancollege.ca>
2024-09-16 14:08:20 -0400

    added the footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+import {DatePipe} from "@angular/common";
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    DatePipe
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+  today: Date = new Date();
+  @Input() name: string = "Sheridan College";
+
+}
